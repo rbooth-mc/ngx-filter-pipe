cf84588a000b79e97b51b2c552af1aa863ae3c47
Randall Booth <rbooth@mastercontrol.com>
2023-07-28 14:07:22 -0600

    Update supported angular version to 15 (#2)

--- src/app/shared/mdl.ts
+++ src/app/shared/mdl.ts
@@ -2,11 +2,13 @@
  * Created by vadimdez on 15/07/16.
  */
 import { Directive, AfterViewChecked } from '@angular/core';
-declare var componentHandler: any;
+let componentHandler: any;
 
 @Directive({
+  // eslint-disable-next-line @angular-eslint/directive-selector
   selector: '[mdl]'
 })
+// eslint-disable-next-line @angular-eslint/directive-class-suffix
 export class MDL implements AfterViewChecked {
   ngAfterViewChecked() {
     componentHandler.upgradeAllRegistered();
